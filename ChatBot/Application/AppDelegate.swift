683ab2b078db99dcb23782a085503578cee6190d
Pablo Roca Rozas <pablorocar@gmail.com>
2016-05-03 01:33:29 +0100

    v

--- ChatBot/Application/AppDelegate.swift
+++ ChatBot/Application/AppDelegate.swift
@@ -70,7 +70,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
+      PR2CoreDataStack.sharedInstance.saveContext()
+   }
 
    // MARK: - Appearance
    
